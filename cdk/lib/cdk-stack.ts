3c48ffeb9ec6338daf09ca36011907d67b12ccb9
tadakun8 <tadakun8.lab@gmail.com>
2022-02-11 18:01:34 +0900

    feat: Create schedule rule

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as logs from "@aws-cdk/aws-logs";
+import * as events from "@aws-cdk/aws-events";
+import * as eventsTarget from "@aws-cdk/aws-events-targets";
 
 import { CONSTANTS } from "./constants";
 
@@ -19,9 +21,24 @@ export class ScheduledKmsCleaningLambdaStack extends cdk.Stack {
         code: lambda.Code.fromAsset("lambda"),
         // hander loaded from "handler" method of index.ts
         handler: "index.handler",
-        // if needed, you can change the value
+        // By default, lambda logs are permanently stored in cloudwatch logs, so specify the retention period.
         logRetention: logs.RetentionDays.ONE_MONTH,
       },
     );
+
+    // Define rules for periodic execution of lambda
+    new events.Rule(this, CONSTANTS.ruleName, {
+      ruleName: CONSTANTS.ruleName,
+      // WARNING: Cron expressions need to be specified in GMT
+      // Specify execution at 4 AM every Monday in JST
+      schedule: events.Schedule.cron({
+        minute: "0",
+        hour: "7",
+        day: "*",
+        month: "SUN",
+        year: "*",
+      }),
+      targets: [new eventsTarget.LambdaFunction(scheduleLambdaFunction)],
+    });
   }
 }
--- cdk/lib/constants.ts
+++ cdk/lib/constants.ts
@@ -3,4 +3,8 @@ export const CONSTANTS = {
    * Name of the lambda to clean kms
    */
   lambdaName: "schedule-kms-cleaning-lambda",
+  /**
+   * Name of schedule rule
+   */
+  ruleName: "execute-kms-cleaning-lambda",
 };
