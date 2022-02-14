58af4d62f870f5a5201fee70d913d3a73c16a0b5
tadakun8 <tadakun8.lab@gmail.com>
2022-02-14 14:26:01 +0900

    fix: Modify to allow typescript code to be deployed as-is.

--- cdk/lambda/index.ts
+++ cdk/lambda/index.ts
@@ -1,3 +1,8 @@
-exports.handler = (event: any, context: any, callback: Function) => {
-  console.log("Hello world");
+import { Context, ScheduledEvent, ScheduledHandler } from "aws-lambda";
+
+export const handler: ScheduledHandler = async (
+  event: ScheduledEvent,
+  context: Context,
+) => {
+  console.log("hello world");
 };
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as lambdaNodejs from "@aws-cdk/aws-lambda-nodejs";
+
 import * as logs from "@aws-cdk/aws-logs";
 import * as events from "@aws-cdk/aws-events";
 import * as eventsTarget from "@aws-cdk/aws-events-targets";
@@ -10,23 +12,25 @@ export class ScheduledKmsCleaningLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create schedule lambda function
-    const scheduleLambdaFunction = new lambda.Function(
+    /**
+     * Define scheduled lambda
+     */
+    const scheduleLambdaFunction = new lambdaNodejs.NodejsFunction(
       this,
       CONSTANTS.lambdaName,
       {
         functionName: CONSTANTS.lambdaName,
-        runtime: lambda.Runtime.NODEJS_14_X,
-        // code loaded from the "lambda" directory,
-        code: lambda.Code.fromAsset("lambda"),
-        // hander loaded from "handler" method of index.ts
-        handler: "index.handler",
+        // Code loaded from the "lambda/index.ts" file
+        // Load the "handler" function by default.
+        entry: "lambda/index.ts",
         // By default, lambda logs are permanently stored in cloudwatch logs, so specify the retention period.
         logRetention: logs.RetentionDays.ONE_MONTH,
       },
     );
 
-    // Define rules for periodic execution of lambda
+    /**
+     * Define rules for periodic execution of lambda
+     */
     new events.Rule(this, CONSTANTS.ruleName, {
       ruleName: CONSTANTS.ruleName,
       // WARNING: Cron expressions need to be specified in GMT
