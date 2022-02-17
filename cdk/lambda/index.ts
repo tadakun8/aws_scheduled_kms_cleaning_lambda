23c5cd6387fd21a0d8ec34407e2312e6b4d667a4
tadakun8 <tadakun8.lab@gmail.com>
2022-02-17 11:44:53 +0900

    feat: Create cleanup lambda

--- cdk/lambda/index.ts
+++ cdk/lambda/index.ts
@@ -1,8 +1,51 @@
+import * as AWS from "aws-sdk";
 import { Context, ScheduledEvent, ScheduledHandler } from "aws-lambda";
+
+/**
+ * Define kms client
+ */
+const kmsClient: AWS.KMS = new AWS.KMS({
+  region: "ap-northeast-1",
+});
 
 export const handler: ScheduledHandler = async (
   event: ScheduledEvent,
   context: Context,
 ) => {
-  console.log("hello world");
+  handlerMethod();
+};
+
+const handlerMethod = async () => {
+  try {
+    const listAliasesResponse = await kmsClient.listAliases().promise();
+    const attachedAliaskeyList = listAliasesResponse.Aliases?.map(
+      (aliasInfo) => aliasInfo.TargetKeyId,
+    ).filter((value) => value !== undefined);
+
+    // keyの一覧を取得
+    const listKeyResponse = await kmsClient.listKeys().promise();
+
+    // 削除保留中になっていないものかつエイリアスがついていないものを判別
+    for (const key of listKeyResponse.Keys!) {
+      const describeKeyResponse = await kmsClient
+        .describeKey({
+          KeyId: key.KeyId!,
+        })
+        .promise();
+      if (
+        describeKeyResponse.KeyMetadata?.KeyState !== "PendingDeletion" &&
+        !attachedAliaskeyList?.includes(key.KeyId)
+      ) {
+        console.log("this is: " + key.KeyId);
+        await kmsClient.scheduleKeyDeletion({
+          KeyId: key.KeyId!,
+          PendingWindowInDays: 7,
+        });
+        console.log("Delete !");
+      }
+    }
+  } catch (err) {
+    // For debug
+    console.log(err);
+  }
 };
